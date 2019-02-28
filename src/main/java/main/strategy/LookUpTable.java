f9f76137d156c6e9cae75353b796ffda4c3f7660
Kevin Sels <sels.kevin@gmail.com>
2019-02-28 21:23:34 +0100

    lookup 1 tag match

--- src/main/java/main/strategy/LookUpTable.java
+++ src/main/java/main/strategy/LookUpTable.java
@@ -14,16 +14,39 @@ public class LookUpTable implements Solution {
 
     @Override
     public List<Slide> execute(List<Photo> photos) {
-        ArrayList<Slide> slides = generateSlides(photos);
+        List<Slide> output = new ArrayList<>();
+        ArrayList<Slide> slides = new ArrayList<>(generateSlides(photos));
         Map<String, List<Slide>> tagMap = calculateTagMap(slides);
         System.out.println("tagMap = " + tagMap.size());
-        return slides;
+
+        Slide slide = slides.get(0);
+        while (!slides.isEmpty()) {
+//            if(slides.size() %100 == 0)
+                System.out.println("slides.size() = " + slides.size());
+            slides.remove(slide);
+            for (String s : slide.getTags()) {
+                tagMap.get(s).remove(slide);
+            }
+            output.add(slide);
+
+            String tag = slide.getTags().iterator().next();
+            List<Slide> overlappingSlides = tagMap.get(tag);
+            if (!overlappingSlides.isEmpty()) {
+                slide = overlappingSlides.iterator().next();
+            } else {
+                System.out.println("no overlap");
+                if (!slides.isEmpty())
+                    slide = slides.get(0);
+            }
+        }
+
+        return output;
     }
 
     private static Map<String, List<Slide>> calculateTagMap(ArrayList<Slide> slides) {
         Map<String, List<Slide>> slidesByTag = new HashMap<>();
         for (Slide slide : slides) {
-            for(String tag: slide.getTags()) {
+            for (String tag : slide.getTags()) {
                 slidesByTag.computeIfAbsent(tag, t -> new ArrayList<>()).add(slide);
             }
         }
@@ -51,4 +74,5 @@ public class LookUpTable implements Solution {
     private static List<Slide> getHorizontals(List<Photo> photos) {
         return photos.stream().filter(Photo::isHorizontal).map(Slide::new).collect(toList());
     }
+
 }
