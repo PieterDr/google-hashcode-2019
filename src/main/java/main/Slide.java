a98da258cd99f8608acd5bdae4c89cdbe6eeb985
PieterDr <pieter.drijkoningen@vrt.be>
2019-02-28 19:08:03 +0100

    Add score calculating method

--- src/main/java/main/Slide.java
+++ src/main/java/main/Slide.java
@@ -1,6 +1,13 @@
 package main;
 
+import com.google.common.collect.Sets;
+
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+
+import static java.lang.Math.min;
+import static java.util.stream.Collectors.toSet;
 
 public class Slide {
 
@@ -10,5 +17,19 @@ public class Slide {
         this.photos = photos;
     }
 
+    int score(Slide o) {
+        Sets.SetView<String> intersection = Sets.intersection(getAllTags(), o.getAllTags());
+        Set<String> thisTags = getAllTags();
+        Set<String> otherTags = o.getAllTags();
+        thisTags.removeAll(intersection);
+        otherTags.removeAll(intersection);
+        return min(intersection.size(), min(thisTags.size(), otherTags.size()));
+    }
 
+    private Set<String> getAllTags() {
+        return photos.stream()
+                .map(photo -> photo.tags)
+                .flatMap(Collection::stream)
+                .collect(toSet());
+    }
 }
